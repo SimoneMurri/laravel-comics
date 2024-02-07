2f22467ed527d03bf2dd92d6bfa2bc9c9b41c4a9
Simone Murri <simonemurri701@gmail.com>
2024-02-07 15:18:02 +0100

    dc-comics header

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,15 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('title', 'Laravel')</title>
     @vite('resources/js/app.js')
+
+    @yield('head-content')
 </head>
 <body>
-    <h1 class="mb-5">
-        ciao
-    </h1>
-    <p>
-        hello world
-    </p>
+    @include('partials.header')
+
+    <main>
+        @yield('main-content')
+    </main>
+
+    @include('partials.footer')
+
+    @yield('script-content')
 </body>
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,61 @@
+<header class="text-center">
+    </section>
+    <section class="navigation">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <a class="navbar-brand" href="#">
+      <img src="{{ Vite::asset ('resources/images/dc-logo.png') }}" alt="Bootstrap" width="30" height="24">
+        </a>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">CHARACHTERS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">COMICS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">MOVIES</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">TV</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">GAMES</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">COLLECTIBLES</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">VIDEOS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">FANS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">NEWS</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            SHOP
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2 border" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </section>
+    <div>
+        <img class="img-jumbo" src="{{ Vite::asset ('resources/images/jumbotron.jpg') }}" alt="">
+    </div>
+</header>
